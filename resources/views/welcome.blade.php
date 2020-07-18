8b71cb9fe2fe633b0069bd7f6a0b9792cd05b97e
Nurzhan <nurzhan.duisenbayev@uco.kz>
2020-07-19 01:35:59 +0600

    Laravel Auth-Reg 5.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,8 @@
         <!-- Styles -->
         <style>
             html, body {
+                background-image: url("{{asset('images/main.jpg')}}");
+                background-size: 100% 100%;
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
@@ -61,44 +63,54 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            #menu-links a{
+                text-decoration: none;
+                color: #0b3e6f;
+                padding: 5px;
+                margin-right: 40px;
+                border: 1px solid darkblue;
+            }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div class="top-right links" id="menu-links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        @if(Auth::user()->isDisabled())
+                            <strong><a href="{{url('/')}}" >Главная</a></strong>
+                        @elseif(Auth::user()->isUser())
+                            <strong><a href="{{url('/user/index')}}" >Кабинет</a></strong>
+                            <strong><a href="{{url('/')}}" >Главная</a></strong>
+                        @elseif(Auth::user()->isVisitor())
+                            <strong><a href="{{url('/')}}" >Главная</a></strong>
+                        @elseif(Auth::user()->isAdministrator())
+                            <strong><a href="{{url('/admin/index')}}" >Панель Администратора</a></strong>
+                            <strong><a href="{{url('/')}}" >Главная</a></strong>
+                        @endif
+                        <strong>
+                            <a  href="{{route('/logout')}}" class="dropdown-item"
+                                onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                Выйти
+                            </a>
+                        </strong>
+                        <form action="{{route('logout')}}" id="logout-form" method="post" style="display: none;">
+                            @csrf
+                        </form>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <strong>
+                            <a href="{{ route('login') }}">Войти</a>
+                        </strong>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <strong>
+                                    <a href="{{ route('register') }}">Регистрация</a>
+                            </strong>
                         @endif
                     @endauth
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-{{--                    @php--}}
-{{--                        $p = \App\SBlog\Core\BlogApp::get_instance()->getProperty('admin_email');--}}
-{{--                        dd($p);--}}
-{{--                    @endphp--}}
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
         </div>
     </body>
 </html>
